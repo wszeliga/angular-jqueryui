21e28f87ad0304922433938c16796543723ef21a
wszeliga <wszeliga@gmail.com>
2013-07-30 00:05:40 -0400

    got the isolated scope working and will have unit tests shortly.

--- app/directives/draggable.js
+++ app/directives/draggable.js
@@ -6,6 +6,10 @@
     jqueryUi.directive('draggable', function() {
         return {
             restrict: 'A',
+            replace: true,
+            transclude: true,
+            scope: {dragOptions:'='},
+            template: '<div data-ng-transclude></div>',
             link: function (scope, element) {
                 element.draggable(scope.dragOptions);
             }
--- app/directives/droppable.js
+++ app/directives/droppable.js
@@ -6,6 +6,10 @@
     jqueryUi.directive('droppable', function() {
         return {
             restrict: 'A',
+            replace: true,
+            transclude: true,
+            scope: { dropOptions: '=' },
+            template: '<div data-ng-transclude></div>',
             link: function(scope, element) {
                 element.droppable(scope.dropOptions);
             }
